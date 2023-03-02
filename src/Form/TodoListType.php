b076bdb23fd5385d2d97a3bbfff59d950f440878
kiaramm10 <kiaramalasquez5@gmail.com>
2023-03-02 10:42:21 +0100

    second update

--- src/Form/TodoListType.php
+++ src/Form/TodoListType.php
@@ -6,13 +6,17 @@ use App\Entity\TodoList;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class TodoListType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Asignatura')
+            ->add('Asignatura', TextType::class, [
+                'attr' => ['class' => 'materia'],
+            ])
             ->add('Turno')
             ->add('Tiempo')
             ->add('Observarciones')
